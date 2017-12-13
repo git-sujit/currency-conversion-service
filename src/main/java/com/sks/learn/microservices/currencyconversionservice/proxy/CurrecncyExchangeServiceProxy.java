f4045c7af394616fceded19c0c51db32aa7af2cc
git-sujit <sujit.cse.nitt@gmail.com>
2017-12-13 09:38:53 -0800

    Ribbon load balancing

--- src/main/java/com/sks/learn/microservices/currencyconversionservice/proxy/CurrecncyExchangeServiceProxy.java
+++ src/main/java/com/sks/learn/microservices/currencyconversionservice/proxy/CurrecncyExchangeServiceProxy.java
@@ -1,12 +1,15 @@
 package com.sks.learn.microservices.currencyconversionservice.proxy;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sks.learn.microservices.currencyconversionservice.model.ConversionValue;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrecncyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
